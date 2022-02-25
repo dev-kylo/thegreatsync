b1e8fafcb96ed09bccb7c38a597f56c29972058a
Kyle <kyle@hitpixel.com>
2022-02-25 16:16:11 +0400

    finish bg

--- pages/components/layout/Background.tsx
+++ pages/components/layout/Background.tsx
@@ -1,16 +1,34 @@
 import styled from "@emotion/styled";
 import tw from "twin.macro";
 
+const Frame = styled.div`
+  ${tw`
+    h-screen
+    bg-red-200
+  `}
+`
+
+const NavBar = styled.div`
+  ${tw`
+    h-32 
+    bg-black 
+    p-6
+  `}
+`;
 
 
 const Main = styled.main`
     background: linear-gradient(71.94deg, #03143F 11.49%, #008579 98.33%);
+    ${tw`
+        min-w-min 
+        min-h-screen
+  `}
 `
 
 const Background = () => {
 
     return (
-        <Main tw="min-w-min min-h-screen">
+        <Main>
             <p>Content will go here.</p>
         </Main>
     )
