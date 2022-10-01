d2d6140aea1ab5d91083cfb79e16152f3a9015f2
Kyle <kyle@hitpixel.com>
2022-10-01 15:58:00 +0400

    switch panel direction

--- components/ui/SlideOver.tsx
+++ components/ui/SlideOver.tsx
@@ -27,15 +27,15 @@ function SlideOver({ children, open, setOpen }: SlideOverProps) {
 
                 <div className="fixed inset-0 overflow-hidden">
                     <div className="absolute inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                        <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full pr-10">
                             <Transition.Child
                                 as={Fragment}
                                 enter="transform transition ease-in-out duration-500 sm:duration-700"
-                                enterFrom="translate-x-full"
+                                enterFrom="-translate-x-full"
                                 enterTo="translate-x-0"
                                 leave="transform transition ease-in-out duration-500 sm:duration-700"
                                 leaveFrom="translate-x-0"
-                                leaveTo="translate-x-full"
+                                leaveTo="-translate-x-full"
                             >
                                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
                                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
