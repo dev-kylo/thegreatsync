1536126d4a558b4f1119623a2b1e10db38613b5e
Kyle <kyle@hitpixel.com>
2022-11-30 16:29:52 +0400

    remove database test vals

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,13 +1,17 @@
+const fs = require('fs');
+
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'tgs_test'),
-      user: env('DATABASE_USERNAME', 'tgs_user'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        ca: fs.readFileSync(`${__dirname}/path/to/your/ca-certificate.crt`).toString(),
+      },
     },
   },
 });
--- strapi/dist/config/database.js
+++ strapi/dist/config/database.js
@@ -4,10 +4,10 @@ exports.default = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'tgs_test'),
-            user: env('DATABASE_USERNAME', 'tgs_user'),
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
             password: env('DATABASE_PASSWORD'),
             ssl: env.bool('DATABASE_SSL', false),
         },
