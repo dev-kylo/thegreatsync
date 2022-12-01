a1dba3dd78e446bc09ca9214e77be92bb252c47c
Kyle <kyle@hitpixel.com>
2022-12-01 07:23:08 +0400

    deploy on fly.io

---
+++ strapi/config/production/database.ts
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path')
+
+export default ({ env }) => ({
+    connection: {
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            // ssl: env('DATABASE_SSL', false)
+            ssl: {
+                ca: fs.readFileSync(path.resolve(__dirname, "../../../certificates/prod-ca-2021.crt")).toString(),
+            },
+        },
+    },
+});
