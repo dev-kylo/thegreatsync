48e1f47621f50a7d7d7b3937b4ef5f478449011e
Kyle <kyle@hitpixel.com>
2023-04-25 08:00:32 +0100

    install syntax highlighter

--- frontend/components/layout/ContentBlock.tsx
+++ frontend/components/layout/ContentBlock.tsx
@@ -1,12 +1,41 @@
 /* eslint-disable react/no-children-prop */
 import ReactMarkdown from 'react-markdown'
 import ContainsCodeSnippet from '../ui/ContainsCodeSnippet';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const ContentBlock = ({ md, numbered = false, id }: { md: string, id: number, numbered?: boolean }) => {
+
     return (
         <article id={`md-block:${id} `} className="prose dark:prose-invert prose-lg prose-headings:text-secondary_lightblue mx-auto">
+            
             <ContainsCodeSnippet numbered={numbered} id={id}>
+            {/* <SyntaxHighlighter language="javascript" style={docco}>
                 <ReactMarkdown children={md} />
+                
+            </SyntaxHighlighter> */}
+
+            <ReactMarkdown
+                children={md}
+                components={{
+                code({node, inline, className, children, ...props}) {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                    <SyntaxHighlighter
+                        {...props}
+                        children={String(children).replace(/\n$/, '')}
+                        style={docco}
+                        language={match[1]}
+                        PreTag="div"
+                    />
+                    ) : (
+                    <code {...props} className={className}>
+                        {children}
+                    </code>
+                    )
+                }
+    }}
+  />
 
             </ContainsCodeSnippet>
         </article >
--- frontend/components/ui/ContainsCodeSnippet.tsx
+++ frontend/components/ui/ContainsCodeSnippet.tsx
@@ -1,14 +1,11 @@
 import { useEffect } from 'react';
-import Prism from "prismjs"
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+// import Prism from "prismjs"
+// import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
 
 
 const ContainsCodeSnippet = ({ children, numbered = false, id }: { children: React.ReactNode, numbered?: boolean, id: number }) => {
 
-    useEffect(() => {
-        console.log('Highlight all')
-        Prism.highlightAll()
-    }, [children, id]);
+
 
     return (
         <div id={`codeblock_${id}`} className={`${numbered ? "line-numbers left-[0!important]" : ""} prose-code:text-[#7fdbca] before:prose-code:content-[''] after:prose-code:content-[''] prose-code:bg-[#011627] prose-code:py-1 prose-code:text-lg`}>
