7be35212f9a771c61687a82c92f258c627f8bf7f
Kyle <kyle@hitpixel.com>
2023-05-07 16:51:35 +0400

    register user with order id

--- strapi/dist/src/api/customer/controllers/customer.js
+++ strapi/dist/src/api/customer/controllers/customer.js
@@ -3,35 +3,49 @@
  * A set of functions called "actions" for `purchase`
  */
 Object.defineProperty(exports, "__esModule", { value: true });
+// User will have a checkbox to use an existing account. Just has to supply an email address with orderId to look up, then must login
+// otherwise, provide name, email and password, anything else? Experience Level, Background
 exports.default = {
     register: async (ctx, next) => {
         try {
             const data = ctx.request.body;
             // Find order
             console.log('Finding Order');
-            const order = await strapi.db.query('plugin::users-permissions.user').findOne({
+            const order = await strapi.db.query('api::order.order').findOne({
                 where: { order_id: data.orderId }
             });
             if (!order)
-                throw new Error('No order exists');
+                return ctx.forbidden('No order exists');
             // Check if order already has a user assigned to it
             // Check for an existing user
             console.log('Searching for User');
             let user = await strapi.db.query('plugin::users-permissions.user').findOne({
                 where: { email: data.username }
             });
+            // If no user found but existingAccount param supplied
+            if (!user && data.existingAccount)
+                return ctx.NotFound('No user with this email address was found. Register with a different address.');
+            // If user found but not delcared an existing account
+            if (user && !data.existingAccount)
+                return ctx.MethodNotAllowed('A user account with this address already exists. Supply a new email or select option for existing account');
             // If there is, update User data
-            if (user) {
+            if (user && data.existingAccount) {
                 await strapi.db.query('plugin::users-permissions.user').update({
                     where: { id: user.id },
                     data: {
-                        password: data.password,
+                        // link order to it.
+                        blocked: true,
                     },
                 });
                 console.log('User Updated');
+                return ctx.body = {
+                    success: true,
+                    message: 'Your account has been updated with the new course. Proceed to login.'
+                };
             }
             else {
                 // Create new User
+                console.log('Creating new user');
                 user = await strapi.plugins['users-permissions'].services.user.add({
                     blocked: false,
                     confirmed: true,
@@ -44,12 +58,15 @@ exports.default = {
                     role: 1 //role id
                 });
                 console.log('NEW USER CREATED');
+                return ctx.body = {
+                    success: true,
+                    message: 'You have successfully registered. Proceed to login.'
+                };
             }
-            ctx.body = {
-                success: true,
-            };
         }
         catch (err) {
+            console.log('Caught');
+            console.log(err);
             ctx.body = err;
         }
     },
