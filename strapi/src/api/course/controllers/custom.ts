60e9bad717c2411d2f804257326ad4d704088fce
Kyle <kyle@hitpixel.com>
2023-05-09 19:18:21 +0400

    fetch specific courses for user

---
+++ strapi/src/api/course/controllers/custom.ts
@@ -0,0 +1,24 @@
+
+
+const { sanitize } = require('@strapi/utils');
+
+
+export default {
+    findByUser: async (ctx, next) => {
+        const { id: userId } = ctx.state.user;
+        
+        // const enrolments = await strapi.query('enrollment').find({ 'user.id': userId });
+        const enrolments =  await strapi.db.query('api::enrollment.enrollment').findMany({
+            where: {  user: userId },
+            populate: ['course']
+          }) 
+
+        const courses = enrolments.map(enrolment => enrolment.course);
+        const unresolved = courses.map(async (course) => await sanitize.contentAPI.output(course));
+        const sanitizedCourses = await Promise.all(unresolved);
+        ctx.response.body = sanitizedCourses;
+
+        await next();
+    }
+}
+        
--- strapi/src/api/release/controllers/release.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  release controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::release.release');
