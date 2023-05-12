b782ab48b69af9acf02206d6f640aba283026b3f
Kyle <kyle@hitpixel.com>
2023-05-12 17:14:06 +0400

    create userCourseProgress entity

---
+++ strapi/dist/src/api/user-course-progress/controllers/user-course-progress.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ *  user-course-progress controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::user-course-progress.user-course-progress');
---
+++ strapi/dist/src/api/user-course-progress/routes/user-course-progress.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * user-course-progress router.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::user-course-progress.user-course-progress');
