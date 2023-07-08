efdf14f32a7ab907d27cd9078ca45d6c0c4d2e17
Kristine <kristine@hitpixel.com>
2023-07-08 18:07:32 +0400

    copy button

--- frontend/components/layout/screens/Text_Image_Code.tsx
+++ frontend/components/layout/screens/Text_Image_Code.tsx
@@ -5,6 +5,7 @@ import ContentBlock from '../ContentBlock';
 import { ImageComp, ResourceLink } from '../../../types';
 import useResponsivePanes from '../../../hooks/useResponsivePanes';
 import PaneTabs from '../PaneTabs';
+import CopyButton from '../../ui/CopyButton';
 
 type Text_Image_Code_Props = {
     text: string;
@@ -19,6 +20,7 @@ export default function Text_Image_Code({ text, code, image, id, heading, links 
     const { isMobile, visiblePane, setVisiblePane } = useResponsivePanes();
     const { url, placeholder } = image.data.attributes;
 
+    console.log(code);
     if (isMobile)
         return (
             <div className="p-4 pt-16 relative">
@@ -92,6 +94,7 @@ export default function Text_Image_Code({ text, code, image, id, heading, links 
                             <Block outerClasses="bg-code_bg" enableScroll>
                                 <ContentBlock md={code} id={id} />
                             </Block>
+                            <CopyButton textcode={code} />
                         </div>
                     </Allotment.Pane>
                 )}
---
+++ frontend/components/ui/CopyButton.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const CopyButton = ({ textcode }: { textcode: string }) => {
+    const [copied, setCopied] = useState(false);
+    const cleanedCode = textcode.replace(/```js|```/g, '');
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(cleanedCode);
+        setCopied(true);
+
+        // Reset the "copied" state after 3 seconds
+        setTimeout(() => {
+            setCopied(false);
+        }, 3000);
+    };
+
+    return (
+        <div className="absolute bottom-4 right-4">
+            <button
+                className="rounded-[4px] border border-l-stone-100 text-white px-5 py-1.5 transition ease-in-out hover:bg-[#008579]"
+                type="button"
+                onClick={copyToClipboard}
+            >
+                {copied ? 'Copied!' : 'Copy'}
+            </button>
+        </div>
+    );
+};
+
+export default CopyButton;
