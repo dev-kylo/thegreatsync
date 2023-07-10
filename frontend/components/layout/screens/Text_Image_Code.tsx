76c9e0bc764973d944950f4ad8850c8a886c9bdf
Tan <41124097+tanthemess@users.noreply.github.com>
2023-07-10 10:23:48 +0400

    Copy Button (#14)
    * copy button
    
    * responsive
    
    * pr changes
    
    * bugfixes
    
    ---------
    
    Co-authored-by: Kristine <kristine@hitpixel.com>
    Co-authored-by: Kyle <kyle@hitpixel.com>

--- frontend/components/layout/screens/Text_Image_Code.tsx
+++ frontend/components/layout/screens/Text_Image_Code.tsx
@@ -5,6 +5,7 @@ import ContentBlock from '../ContentBlock';
 import { ImageComp, ResourceLink } from '../../../types';
 import useResponsivePanes from '../../../hooks/useResponsivePanes';
 import PaneTabs from '../PaneTabs';
+import CopyButton from '../../ui/CopyButton';
 
 type Text_Image_Code_Props = {
     text: string;
@@ -19,6 +20,7 @@ export default function Text_Image_Code({ text, code, image, id, heading, links 
     const { isMobile, visiblePane, setVisiblePane } = useResponsivePanes();
     const { url, placeholder } = image.data.attributes;
 
+    console.log(code);
     if (isMobile)
         return (
             <div className="p-4 pt-16 relative">
@@ -47,10 +49,11 @@ export default function Text_Image_Code({ text, code, image, id, heading, links 
                     </div>
                 )}
                 {visiblePane === 'code' && (
-                    <div className="bg-violet-800 h-full ">
+                    <div className="bg-violet-800 h-full relative">
                         <Block outerClasses="bg-code_bg" enableScroll>
                             <ContentBlock md={code} id={id} />
                         </Block>
+                        <CopyButton textcode={code} />
                     </div>
                 )}
             </div>
@@ -92,6 +95,7 @@ export default function Text_Image_Code({ text, code, image, id, heading, links 
                             <Block outerClasses="bg-code_bg" enableScroll>
                                 <ContentBlock md={code} id={id} />
                             </Block>
+                            <CopyButton textcode={code} />
                         </div>
                     </Allotment.Pane>
                 )}
