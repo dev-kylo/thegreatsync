38dd4eea86688add2c5d151fb18d1e158da5801c
Kristine <kristine@hitpixel.com>
2023-07-18 19:02:53 +0400

    hide hamburger menu

--- frontend/components/ui/Navbar.tsx
+++ frontend/components/ui/Navbar.tsx
@@ -12,25 +12,31 @@ export default function Navbar({
     subChapterTitle,
     menuData,
     current,
+    pageType,
 }: {
     chapterTitle: string;
     subChapterTitle: string;
     current: CurrentLocation;
     menuData?: MenuItem[];
+    pageType?: string;
 }) {
     const [openMenu, setOpenMenu] = useState(false);
+
+    console.log(pageType);
     return (
         <>
             <Disclosure as="nav" className="flex-shrink-0 bg-primary_blue">
                 <div className="mx-auto max-w-8xl px-2 sm:px-4 lg:px-8 h-full flex justify-center flex-col">
                     <div className="relative flex items-center justify-between">
                         {/* Course Menu Button */}
-                        <Bars3Icon
-                            onClick={() => setOpenMenu(true)}
-                            className="block h-8 w-8 xl:h-10 xl:w-10 text-white hover:cursor-pointer hover:text-primary_green focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary_blue"
-                            aria-hidden="true"
-                        />
-                        <TitleStrip chapterTitle={chapterTitle} subChapterTitle={subChapterTitle} />
+                        {pageType !== 'listing' && (
+                            <Bars3Icon
+                                onClick={() => setOpenMenu(true)}
+                                className="block h-8 w-8 xl:h-10 xl:w-10 text-white hover:cursor-pointer hover:text-primary_green focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary_blue"
+                                aria-hidden="true"
+                            />
+                        )}
+                        <TitleStrip chapterTitle={chapterTitle} subChapterTitle={subChapterTitle} pageType={pageType} />
                         <div className="block">
                             <div className="flex items-center justify-end">
                                 <ProfileDropDown />
--- frontend/components/ui/TitleStrip.tsx
+++ frontend/components/ui/TitleStrip.tsx
@@ -1,5 +1,19 @@
-const TitleStrip = ({ chapterTitle, subChapterTitle }: { chapterTitle: string; subChapterTitle: string }) => (
-    <div className="flex flex-col items-end md:items-center justify-center">
+const TitleStrip = ({
+    chapterTitle,
+    subChapterTitle,
+    pageType,
+}: {
+    chapterTitle: string;
+    subChapterTitle: string;
+    pageType?: string;
+}) => (
+    <div
+        className={`flex flex-col ${
+            pageType !== 'listing'
+                ? 'items-end md:items-center justify-center'
+                : 'items-start md:items-start justify-start'
+        }`}
+    >
         <span className="text-secondary_lightblue text-md m-0 p-0 md:text-xl block">{chapterTitle}</span>
         <span className=" m-0 mt-1 p-0 text-right md:text-center text-xs block uppercase text-green-400 font-bold">
             {subChapterTitle}
