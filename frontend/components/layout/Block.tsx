5f42f8e45897ebc7cd5f45b2998372c8b287335f
Kyle <kyle@hitpixel.com>
2023-07-24 22:16:04 +0400

    bugfix: scroll error

--- frontend/components/layout/Block.tsx
+++ frontend/components/layout/Block.tsx
@@ -1,5 +1,6 @@
 type BlockProps = {
     children: React.ReactNode;
+
     outerClasses?: string;
     innerClasses?: string;
     enableScroll?: boolean;
@@ -7,21 +8,23 @@ type BlockProps = {
     id?: string;
 };
 
-const Block = ({ children, outerClasses, innerClasses, enableScroll, hideBorder, id }: BlockProps) => (
-    <div
-        id={id}
-        className={`h-full w-full relative overflow-hidden rounded-lg ${
-            hideBorder ? '' : 'border-2 border-r-2 border-secondary_lightblue'
-        } ${enableScroll ? 'scrollbar-thin scrollbar-thumb-primary_green overflow-y-scroll' : ''} ${outerClasses}`}
-    >
-        {enableScroll ? (
-            <div className={`absolute top-0 left-0 w-full h-auto ${innerClasses}`}>
-                <div className="px-2 block relative">{children}</div>
-            </div>
-        ) : (
-            <div>{children}</div>
-        )}
-    </div>
-);
+const Block = ({ children, outerClasses, innerClasses, enableScroll, hideBorder, id }: BlockProps) => {
+    return (
+        <div
+            id={id}
+            className={`h-full w-full relative overflow-hidden rounded-lg ${
+                hideBorder ? '' : 'border-2 border-r-2 border-secondary_lightblue'
+            } ${enableScroll ? 'scrollbar-thin scrollbar-thumb-primary_green overflow-y-scroll' : ''} ${outerClasses}`}
+        >
+            {enableScroll ? (
+                <div className={`absolute top-0 left-0 w-full h-auto ${innerClasses}`}>
+                    <div className="px-2 block relative">{children}</div>
+                </div>
+            ) : (
+                <div>{children}</div>
+            )}
+        </div>
+    );
+};
 
 export default Block;
