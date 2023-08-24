664c67b676c6975de6cc63dc1f8a0ce44150897e
Kyle Robinson <30776816+kylorobs@users.noreply.github.com>
2023-08-24 20:44:32 +0400

    Release/1.11.8 (#57)
    * redirect to not allowed page from home
    
    * h2 formatting
    
    * h2 formatting cont
    
    * add cursor on panel edge
    
    * fallback cursor
    
    ---------

--- frontend/components/layout/ContentBlock.tsx
+++ frontend/components/layout/ContentBlock.tsx
@@ -15,21 +15,25 @@ const ContentBlock = ({
     id,
     heading,
     links,
-    type,
+    textType,
 }: {
     md: string;
     id: number;
     heading?: string;
-    type?: 'text';
+    textType?: 'page' | 'block';
     links?: ResourceLink[];
 }) => {
     return (
         <article
-            id={`md-${type === 'text' ? 'carticle' : 'code'}-block`}
-            key={`md-${type === 'text' ? 'article' : 'code'}-block:${id}`}
+            id={`md-${textType ? 'carticle' : 'code'}-block`}
+            key={`md-${textType ? 'article' : 'code'}-block:${id}`}
             className={`prose ${
-                type !== 'text' ? 'h-full pb-2' : 'pb-16'
-            } prose-xl prose-h2:text-3xl prose-h1:text-4xl prose-h3:text-2xl prose-strong:text-[#c792ea] prose-strong:font-extrabold prose-li:text-offwhite prose-span:text-offwhite  prose-a:text-green-500 prose-em:text-offwhite prose-p:text-offwhite prose-headings:text-secondary_lightblue mx-auto prose-pre:p-0 pt-2 prose-code:text-[#7fdbca] prose-code:font-mono prose-code:after:hidden prose-code:before:hidden`}
+                !textType
+                    ? 'h-full pb-2'
+                    : textType === 'page'
+                    ? 'pb-16 prose-h1:text-5xl prose-h2:pt-12'
+                    : 'pb-16 prose-h1:text-[2.5rem] '
+            } prose-xl prose-h2:text-4xl prose-h3:text-2xl prose-strong:text-[#c792ea] prose-strong:font-extrabold prose-li:text-offwhite prose-span:text-offwhite  prose-a:text-green-500 prose-em:text-offwhite prose-p:text-offwhite prose-headings:text-secondary_lightblue mx-auto prose-pre:p-0 pt-2 prose-code:text-[#7fdbca] prose-code:font-mono prose-code:after:hidden prose-code:before:hidden`}
         >
             {heading && <h1>{heading}</h1>}
 
--- frontend/components/layout/screens/Text_Image.tsx
+++ frontend/components/layout/screens/Text_Image.tsx
@@ -42,7 +42,7 @@ export default function Text_Image({ text, image, id, heading, links, imageAlt }
                                 heading={heading}
                                 links={links}
                                 key={`mobiletext-block:-${id}`}
-                                type="text"
+                                textType="block"
                             />
                         </Block>
                     </div>
@@ -77,7 +77,7 @@ export default function Text_Image({ text, image, id, heading, links, imageAlt }
                 <Allotment.Pane>
                     <div id="one" className="bg-black h-full">
                         <Block outerClasses="bg-code_bg" innerClasses="p-4" enableScroll key={`text-block:-${id}`}>
-                            <ContentBlock md={text} id={id} heading={heading} links={links} type="text" />
+                            <ContentBlock md={text} id={id} heading={heading} links={links} textType="block" />
                         </Block>
                     </div>
                 </Allotment.Pane>
