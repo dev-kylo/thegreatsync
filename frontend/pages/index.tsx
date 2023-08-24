94464a0e494f0424c898e472163f6dde30356e15
Kyle <kyle@hitpixel.com>
2023-08-24 18:51:49 +0400

    redirect to not allowed page from home

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -30,10 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     if (!resp || resp.status !== 200) {
         if (!resp) return serverRedirectObject(`/error?redirect=${context.resolvedUrl}&error=500`);
         if (resp.status === 401) return serverRedirectObject(`/signin?redirect=${context.resolvedUrl}`);
-        if (resp.status === 403)
-            return serverRedirectObject(
-                `/error?redirect=${context.resolvedUrl}&error='You do not have the correct permissions to view this course'`
-            );
+        if (resp.status === 403) return serverRedirectObject(`/notallowed`);
         if (resp.status === 500)
             return serverRedirectObject(
                 `/error?redirect=${context.resolvedUrl}&error='Oh no, the server seems to be down!'`
