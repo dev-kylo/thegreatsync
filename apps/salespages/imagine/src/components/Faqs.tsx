2fcb29c931a2155f6ae1377f1fb71a8e60df8b7f
devkylo <kylerobbo12@gmail.com>
2025-09-28 19:29:58 +0400

    remove landing app

--- apps/salespages/imagine/src/components/Faqs.tsx
+++ apps/salespages/imagine/src/components/Faqs.tsx
@@ -20,29 +20,29 @@ const faqs = [
       "For beginners and intermediates, but not for complete beginners who have never seen JavaScript. It is meant to help you 'level up' your skills, not to learn from scratch.",
   },
   {
-      id: 4,
-      question: "What if I am an absolute beginner?",
-      answer:
-        "This course will still be super valuable to you, but not as your ONLY resource. Use things like Codecademy and freeCodeCamp, and cross reference what you learn with The Great Sync. ",
-    },
-    {
-      id: 5,
-      question: "Do I only look at pictures or do I do any coding?",
-      answer:
-        "The Great Sync is not a shortcut. It's an effective learning method, but nothing beats the hours you put in behind the code editor. You can't learn JavaScript by looking at pictures. I provide code exercises throughout, and every section has a 'Mental Model Applied' section. But it is up to you to practice recognizing and applying the patterns in your own projects.",
-    },
-    {
-      id: 6,
-      question: "Can I get a refund?",
-      answer:
-        "As I am offering this course for a limited time at a highly reduced price, I am not offering refunds. As long as you commit to the course with an open mind, you won't be disappointed. ",
-    },
-    {
-      id: 7,
-      question: "Is this course for me?",
-      answer:
-        "If you find yourself copy pasting a lot of code and relying on AI, and you enjoy learning visually, this course is 100% for you! ",
-    },
+    id: 4,
+    question: "What if I am an absolute beginner?",
+    answer:
+      "This course will still be super valuable to you, but not as your ONLY resource. Use things like Codecademy and freeCodeCamp, and cross reference what you learn with The Great Sync. ",
+  },
+  {
+    id: 5,
+    question: "Do I only look at pictures or do I do any coding?",
+    answer:
+      "The Great Sync is not a shortcut. It's an effective learning method, but nothing beats the hours you put in behind the code editor. You can't learn JavaScript by looking at pictures. I provide code exercises throughout, and every section has a 'Mental Model Applied' section. But it is up to you to practice recognizing and applying the patterns in your own projects.",
+  },
+  {
+    id: 6,
+    question: "Can I get a refund?",
+    answer:
+      "As I am offering this course for a limited time at a highly reduced price, I am not offering refunds. As long as you commit to the course with an open mind, you won't be disappointed. ",
+  },
+  {
+    id: 7,
+    question: "Is this course for me?",
+    answer:
+      "If you find yourself copy pasting a lot of code and relying on AI, and you enjoy learning visually, this course is 100% for you! ",
+  },
 
 ]
   
